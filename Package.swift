4d5120824a761180134b3c3fc96d8b582fef32fe
Eugene <strelnikoveugene@gmail.com>
2024-12-18 04:56:45 -0300

    update

--- Package.swift
+++ Package.swift
@@ -1,28 +1,19 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ElasticApmWrapper",
-    platforms: [
-        .iOS(.v13),
-    ],
     products: [
         .library(
             name: "ElasticApmWrapper",
-            type: .dynamic,
             targets: ["ElasticApmWrapper"]),
     ],
-    dependencies:[
-        .package(name: "apm-agent-ios", url: "https://github.com/elastic/apm-agent-ios.git", from: "1.0.0"),
-    ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "ElasticApmWrapper",
-            dependencies: [
-                .product(name: "ElasticApm", package: "apm-agent-ios")
-            ]
-        ),
+            path: "./ElasticApmWrapper.xcframework"
+        )
     ]
 )
--- Sources/ElasticApmWrapper/ElasticApmWrapper.swift
+++ Sources/ElasticApmWrapper/ElasticApmWrapper.swift
@@ -1,9 +1,2 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-@_implementationOnly import ElasticApm
-
-public class WrapperSizeTest {
-    public init() {
-        ElasticApmAgent.start()
-    }
-}
