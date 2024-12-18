90b86b05968e5a8163e8eac41fa179f8a31d3ea5
Eugene <strelnikoveugene@gmail.com>
2024-12-18 05:40:27 -0300

    update

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ElasticApmWrapper",
-            path: "./ElasticApmWrapper.xcframework"
+            path: "Frameworks/ElasticApmWrapper.xcframework"
         )
     ]
 )
